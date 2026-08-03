04dc647a16aaba0555ba11c41e9b57f7270915f6
Richard Jean-Baptiste <Richinbk1@gmail.com>
2026-08-03 14:40:32 -0400

    first commit

---
+++ src/main/java/com/example/tajappointments/TajappointmentsApplication.java
@@ -0,0 +1,13 @@
+package com.example.tajappointments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TajappointmentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TajappointmentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tajappointments/TajappointmentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tajappointments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TajappointmentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
