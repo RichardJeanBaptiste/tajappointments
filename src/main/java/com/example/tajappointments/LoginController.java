1e6409149015f44a8e6585b0f06edfa8c078a09c
Richard Jean-Baptiste <Richinbk1@gmail.com>
2026-08-03 22:26:44 -0400

    added login/registration api route - connected to pG

--- src/main/java/com/example/tajappointments/LoginController.java
+++ src/main/java/com/example/tajappointments/LoginController.java
@@ -1,16 +1,73 @@
 package com.example.tajappointments;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.ModelAndView;
-
-@Controller
+@RestController
 public class LoginController {
 
-    @GetMapping("/")
-    public ModelAndView index(){
-        return new ModelAndView("index");
+    @PostMapping("/login")
+    public String loginHandler(LoginForm form) {
+
+        String username = form.getLogin_username();
+        String password = form.getLogin_password();
+        return username + " " + password;
     }
 
+    @PostMapping("/registration")
+    public String registrationHandler(RegisterForm form) {
+        String username = form.getRegistration_username();
+        String password = form.getRegistration_password();
+
+        return username + " " + password;
+    }
+
+    @PostMapping("/guest")
+    public void guestHandler() {
+        System.out.println("guest handler");
+    }
 }
+
+class LoginForm {
+    private String login_username;
+    private String login_password;
+
+
+    public void setLogin_username(String login_username) {
+        this.login_username = login_username;
+    }
+
+    public String getLogin_username() {
+        return login_username;
+    }
+
+    public void setLogin_password(String login_password) {
+        this.login_password = login_password;
+    }
+
+    public String getLogin_password() {
+        return login_password;
+    }
+}
+
+class RegisterForm {
+    private String registration_username;
+    private String registration_password;
+
+    public void setRegistration_username(String registration_username) {
+        this.registration_username = registration_username;
+    }
+
+    public String getRegistration_username() {
+        return registration_username;
+    }
+
+    public void setRegistration_password(String registration_password){
+        this.registration_password = registration_password;
+    }
+
+    public String getRegistration_password(){
+        return registration_password;
+    }
+}
+
